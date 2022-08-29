20e50c139812c79819d0ac675b82147baf91e706
koltal <nibrasabubakar@gmail.com>
2022-08-29 20:05:30 +0100

    css modified

--- src/components/projectSection.js
+++ src/components/projectSection.js
@@ -1,13 +1,90 @@
 const Project = () => {
     return ( 
-        <div>
-            <span>Featured Projects</span>
-            <div className= 'Project-Container'>
-                <div>
-                   <span>HRMS</span>
-                   <span>A hospital record management system</span>
+        <div className= 'Project-Container'>
+           <div className='C-My-Projects'>
+                <span className='My-projects'>Featured Projects</span>
+           </div>
+            <div className= 'Project-Card-Container'>
+                <div className='project-card'>
+                   <div>
+                    <span className='project-title'>HRMS</span><br></br>
+                    <p>An electronic hospital record management 
+                        sytem with full functionality
+                    </p>
+                   </div>
+                   <div>
+                    <span>html</span>
+                    <span>css</span>
+                    <span>js</span>
+                   </div>
+                   <div>
+                    <button>Live</button>
+                    <button>Github</button>
+                   </div>
+                </div>
+                <div className='project-card'>
+                   <div>
+                    <span className='project-title'>HRMS</span><br></br>
+                    <p>An electronic hospital record management 
+                        sytem with full functionality
+                    </p>
+                   </div>
+                   <div>
+                    <span>html</span>
+                    <span>css</span>
+                    <span>js</span>
+                   </div>
+                   <div>
+                    <button>Live</button>
+                    <button>Github</button>
+                   </div>
+                </div>
+                <div className='project-card'>
+                   <div>
+                    <span className='project-title'>HRMS</span><br></br>
+                    <p>An electronic hospital record management 
+                        sytem with full functionality
+                    </p>
+                   </div>
+                   <div className='skills-block'>
+                       <div><span>html </span></div> 
+                       <div><span>css </span></div> 
+                       <div><span>js </span></div> 
+                         
+                   </div>
+                   <div className='Btn-container-redirect'>
+                    <button>Live</button>
+                    <button>Github</button>
+                   </div>
                 </div>
             </div>
+            <div className='social'>
+                <div><h2>Lets Connect</h2></div>
+            <div className='social-icons'> 
+                <div className='github'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 6c-3.313 0-6 2.686-6 6 0 2.651 1.719 4.9 4.104 5.693.3.056.396-.13.396-.289v-1.117c-1.669.363-2.017-.707-2.017-.707-.272-.693-.666-.878-.666-.878-.544-.373.041-.365.041-.365.603.042.92.619.92.619.535.917 1.403.652 1.746.499.054-.388.209-.652.381-.802-1.333-.152-2.733-.667-2.733-2.965 0-.655.234-1.19.618-1.61-.062-.153-.268-.764.058-1.59 0 0 .504-.161 1.65.615.479-.133.992-.199 1.502-.202.51.002 1.023.069 1.503.202 1.146-.776 1.648-.615 1.648-.615.327.826.121 1.437.06 1.588.385.42.617.955.617 1.61 0 2.305-1.404 2.812-2.74 2.96.216.186.412.551.412 1.111v1.646c0 .16.096.347.4.288 2.383-.793 4.1-3.041 4.1-5.691 0-3.314-2.687-6-6-6z"/></svg>
+                </div>
+                <div className='instagram'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 24 24"><path d="M14.829 6.302c-.738-.034-.96-.04-2.829-.04s-2.09.007-2.828.04c-1.899.087-2.783.986-2.87 2.87-.033.738-.041.959-.041 2.828s.008 2.09.041 2.829c.087 1.879.967 2.783 2.87 2.87.737.033.959.041 2.828.041 1.87 0 2.091-.007 2.829-.041 1.899-.086 2.782-.988 2.87-2.87.033-.738.04-.96.04-2.829s-.007-2.09-.04-2.828c-.088-1.883-.973-2.783-2.87-2.87zm-2.829 9.293c-1.985 0-3.595-1.609-3.595-3.595 0-1.985 1.61-3.594 3.595-3.594s3.595 1.609 3.595 3.594c0 1.985-1.61 3.595-3.595 3.595zm3.737-6.491c-.464 0-.84-.376-.84-.84 0-.464.376-.84.84-.84.464 0 .84.376.84.84 0 .463-.376.84-.84.84zm-1.404 2.896c0 1.289-1.045 2.333-2.333 2.333s-2.333-1.044-2.333-2.333c0-1.289 1.045-2.333 2.333-2.333s2.333 1.044 2.333 2.333zm-2.333-12c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.958 14.886c-.115 2.545-1.532 3.955-4.071 4.072-.747.034-.986.042-2.887.042s-2.139-.008-2.886-.042c-2.544-.117-3.955-1.529-4.072-4.072-.034-.746-.042-.985-.042-2.886 0-1.901.008-2.139.042-2.886.117-2.544 1.529-3.955 4.072-4.071.747-.035.985-.043 2.886-.043s2.14.008 2.887.043c2.545.117 3.957 1.532 4.071 4.071.034.747.042.985.042 2.886 0 1.901-.008 2.14-.042 2.886z"/></svg>
+                </div>
+                <div className='twitter'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
+                </div>
+                <div className='linkdin'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="54" height="54" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg> 
+                </div>
+            </div>
+            </div>
+            <div className='self-introduction-container'>
+                <div><h2>Get to Know Me</h2></div>
+                <div>
+                    <p>I'm a front end developer with 3 years experience in REACTJS and 2 years experience in REACT NATIVE.I have the passion of working in a tech industry where i develop products for thousands of people to use. In my career of learning, I realise that tech industry is just a continuous learning world, hence I’m always in the process and passionate of learning new things most especially in the javascript world. And finally i'm open for contracts, remote jobs, and jobs that may also need relocation.</p>
+                </div>
+            </div>
+            <div className='my-skills-container'>
+                <div><h2>My Skills</h2></div>
+            </div>
+            
         </div>
      );
 }
